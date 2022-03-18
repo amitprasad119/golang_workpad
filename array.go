46f30f345a0bb9eed9e176845e325cf4c687af25
Amit Prasad <amit.prasadthoughtworks.com@Amits-MacBook-Pro.local>
2022-03-18 13:03:34 +0530

    minor changes

--- array.go
+++ array.go
@@ -4,11 +4,9 @@ import "fmt"
 
 func main() {
 
-	names := []string{"amit", "Sumit", "suman"} // give the fix dimension
+	names := [3]string{"amit", "Sumit", "suman"} // give the fix dimension
 
 	fmt.Println(names)
 	fmt.Println(len(names))
 	fmt.Println(cap(names))
-	appendedArray := append(names, "john")
-	fmt.Println(appendedArray)
 }
