6de592d0c91aff06629e3e600ef36dc7f49bfbc7
Amit Prasad <amit.prasadthoughtworks.com@Amits-MacBook-Pro.local>
2022-03-19 21:34:10 +0530

    map addition

---
+++ map.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// create the map 
+	var ids map[int]string
+   // assign the values to map
+	ids = map[int]string{ 10 : "jack",
+	   20: "peter",
+	   30: "mack",
+	   40: "rock",
+	}
+
+	fmt.Println(ids)
+
+	// delete values from map using key
+
+	delete(ids,30)
+
+	fmt.Println(ids)
+
+	// create/update another entry for map keys 
+
+	ids[50] = "Isaac"
+	ids[40] = "Milton"
+	fmt.Println(ids)
+
+	// create and initialize the map in another way as well 
+
+	var address = make(map[string]string)
+	fmt.Println(address)
+	address["milton"] = "USA"
+	address["issac"] = "UK"
+	fmt.Println(address)
+
+	// get the value of map 
+
+	miltonValue := address["milton"]
+	fmt.Println(miltonValue)
+
+}
