eee30b180c0c8a5f45ba06f8c68f12ece18e4636
Amit <amitprasad.om@gmail.com>
2022-03-21 21:58:19 +0530

    conditional statements

---
+++ ifelse.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt" 
+
+func main() {
+  var num int
+
+  fmt.Println("Enter the number")
+  fmt.Scanf("%d",&num);
+  
+  if num % 2 == 0 {
+    fmt.Printf("Number %d is divisible by 2", num)
+  } else if num % 3 == 0 {
+    fmt.Printf("Number %d is divisible by 3", num)
+  } else {
+	fmt.Println("Number %d is unknown",num)
+  }
+
+}
