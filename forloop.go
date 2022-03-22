39d0b9718c18610a4febaeaa4197d24fd953bb19
Amit <amitprasad.om@gmail.com>
2022-03-22 22:39:15 +0530

    added break and continue example in loop

--- forloop.go
+++ forloop.go
@@ -9,6 +9,30 @@ func main() {
 		} else {
 			fmt.Println("Odd", i)
 		}
-
 	}
+
+	/*
+	 for loop with break
+	 Create a loop and break if loop reach at 5th
+	*/
+	fmt.Println("Example of break with loop")
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			break
+		}
+		fmt.Println(i)
+	}
+
+	/*
+	   for loop with continue
+	   Create a loop and skip if number is even and print the rest
+	*/
+	fmt.Println("Example of continue with loop")
+	for i := 0; i <= 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
 }
