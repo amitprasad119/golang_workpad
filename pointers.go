f6f08dbd4be973eceb7ab35598e53c7728d38a9c
Amit <amitprasad.om@gmail.com>
2022-03-27 15:05:11 +0530

    pointers small example

---
+++ pointers.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+/*
+  b := &a {means var b *int }
+  *b means ( *&a) that is value from the addess location
+*/
+func main() {
+	x := 0
+    passByValue(x)
+	fmt.Println("Pass by values")
+	fmt.Println(x);
+	fmt.Println("Pass by reference/ptr")
+	fmt.Println("address to x" , &x)
+	passByPtr(&x) // pass the address using & symbol to function
+	fmt.Println("Value after changing the data by its addess")
+	fmt.Println(x)
+
+}
+
+func passByValue(y int) {
+	 x := 23
+
+	 fmt.Println(x)
+}
+
+
+func passByPtr(y *int)  { // recieving the variable as pointer to the addess passing value hold 
+	 *y = 45 // on the addess of y change the value to 45 i.e (*&y) 
+	 
+	 fmt.Println("address to passed variable",y)
+	 fmt.Println("Changed value to passed variable inside the passByPtr", *y)
+}
