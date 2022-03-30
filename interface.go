092c692529ae9a698181f73d77414d5e5fb5c170
Amit <amitprasad.om@gmail.com>
2022-03-30 22:31:18 +0530

    interface example with doc

--- interface.go
+++ interface.go
@@ -16,20 +16,20 @@ type Vechile struct {
 	chasisNumber string
 }
 
-func (p Person) show() {
+func (p Person) show() { //overiding show() here from interface Printer
 	fmt.Println("Hey there I'm ", p.fname, p.lname)
 }
 
-func (v Vechile) show() {
+func (v Vechile) show() { //overiding show() here from interface Printer
 	fmt.Println("Vechile is  ", v.model, v.chasisNumber)
 }
 
 func main() {
 	var personPrinter Printer
 	var vechiclePrinter Printer
-	personPrinter = Person{fname: "John", lname: "Moore"}
+	personPrinter = Person{fname: "John", lname: "Moore"} // creating the instance of type Person and give the reference to Printer
 	personPrinter.show()
-	vechiclePrinter = Vechile{model: "FX-78", chasisNumber: "FL90234324G"}
+	vechiclePrinter = Vechile{model: "FX-78", chasisNumber: "FL90234324G"} // creating the instance of type Person and give the reference to Printer
 	vechiclePrinter.show()
 
 }
